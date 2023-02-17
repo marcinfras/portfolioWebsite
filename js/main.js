e6b3a8186b5a6e33eae18e3480a4f71d0a485567
Marcin Fraś <marcinfras2003@gmail.com>
2023-02-17 22:17:58 +0100

    Create message section and footer

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// const header = document.querySelector(".header");
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 0) {
+//     header.classList.add("header--active");
+//   } else {
+//     header.classList.remove("header--active");
+//   }
+// });
+
+// const burger = document.querySelector(".header__burger");
+// const menu = document.querySelector(".menu");
+
+// burger.addEventListener("click", () => {
+//   burger.classList.toggle("header__burger--active");
+//   menu.classList.toggle("menu--active");
+// });
