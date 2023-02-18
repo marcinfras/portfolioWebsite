d657b31e561220ff6c073cb90e9184171196fd42
Marcin Fraś <marcinfras2003@gmail.com>
2023-02-18 16:54:32 +0100

    Add burger and menu scripts in main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
-// const header = document.querySelector(".header");
+const header = document.querySelector(".header");
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".menu");
+const navItems = [...document.querySelectorAll(".menu__link")];
 
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 0) {
-//     header.classList.add("header--active");
-//   } else {
-//     header.classList.remove("header--active");
-//   }
-// });
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.classList.add("header--active");
+  } else {
+    header.classList.remove("header--active");
+  }
+});
 
-// const burger = document.querySelector(".header__burger");
-// const menu = document.querySelector(".menu");
+burger.addEventListener("click", () => {
+  burger.classList.toggle("header__burger--active");
+  menu.classList.toggle("menu--active");
+});
 
-// burger.addEventListener("click", () => {
-//   burger.classList.toggle("header__burger--active");
-//   menu.classList.toggle("menu--active");
-// });
+const scrollToSection = (e) => {
+  const section = document.querySelector(`.${e.target.dataset.key}`);
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+
+  burger.classList.remove("header__burger--active");
+  menu.classList.remove("menu--active");
+};
+
+navItems.forEach((item) => {
+  item.addEventListener("click", scrollToSection);
+});
